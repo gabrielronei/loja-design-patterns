e0cfadaf47649359d4b063b9aacff71e7d734635
Gabriel Ronei <gabriheron@hotmail.com>
2021-02-22 11:55:17 -0300

    Criando a situação do orçamento

--- src/br/com/gabriels/loja/orcamento/Orcamento.java
+++ src/br/com/gabriels/loja/orcamento/Orcamento.java
@@ -6,6 +6,7 @@ public class Orcamento {
 
     private BigDecimal valor;
     private Integer quantidadeItens;
+    private String situacao;
 
     public Orcamento(BigDecimal valor, Integer quantidadeItens) {
         this.valor = valor;
@@ -18,5 +19,21 @@ public class Orcamento {
 
     public Integer getQuantidadeItens() {
         return quantidadeItens;
+    }
+
+    public void aplicarDescontoExtra() {
+        BigDecimal valorDoDescontro = BigDecimal.ZERO;
+        if (situacao.equals("EM_ANALISE")) {
+            valorDoDescontro = new BigDecimal("0.05");
+        } else if (situacao.equals("APROVADO")) {
+            valorDoDescontro = new BigDecimal("0.02");
+        }
+
+        this.valor.subtract(valorDoDescontro);
+    }
+
+
+    public void aprovar() {
+        this.situacao = "APROVADO";
     }
 }
